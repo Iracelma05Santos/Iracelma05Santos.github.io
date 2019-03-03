3a94cbbf76fcd28e48c6749b360a6b46c6bc0395
Iracelma Santos <ira96ribeiro@gmail.com>
2019-03-03 20:34:10 +0200

    Add files via upload

---
+++ js/4.js
@@ -0,0 +1,40 @@
+function one(){
+    document.getElementById('um').style.display="flex";
+    document.getElementById('dois').style.display="none";
+    document.getElementById('tres').style.display="none";
+    document.getElementById('quatro').style.display="none";
+    document.getElementById('cinco').style.display="none";
+
+}
+
+function two(){
+    document.getElementById('um').style.display="none";
+    document.getElementById('dois').style.display="flex";
+    document.getElementById('tres').style.display="none";
+    document.getElementById('quatro').style.display="none";
+    document.getElementById('cinco').style.display="none";
+}
+ 
+function three(){
+    document.getElementById('um').style.display="none";
+    document.getElementById('dois').style.display="none";
+    document.getElementById('tres').style.display="flex";
+    document.getElementById('quatro').style.display="none";
+    document.getElementById('cinco').style.display="none";
+}
+
+function four(){
+    document.getElementById('um').style.display="none";
+    document.getElementById('dois').style.display="none";
+    document.getElementById('tres').style.display="none";
+    document.getElementById('quatro').style.display="flex";
+    document.getElementById('cinco').style.display="none";
+}
+
+function five(){
+    document.getElementById('um').style.display="none";
+    document.getElementById('dois').style.display="none";
+    document.getElementById('tres').style.display="none";
+    document.getElementById('quatro').style.display="none";
+    document.getElementById('cinco').style.display="flex";
+}
---
+++ js/5.js
@@ -0,0 +1,11 @@
+function antes(){
+    document.getElementById('prim').style.display = "none";
+    document.getElementById('seg').style.display = "inline";
+
+}
+
+function agora(){
+    document.getElementById('prim').style.display = "inline";
+    document.getElementById('seg').style.display = "none";
+
+}
